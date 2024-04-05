ef0b93ee27b8d1284018a1c09cd1cbb01bfcadf2
Evgeniy Khlystov <hlystovea@gmail.com>
2024-04-05 16:58:05 +0700

    Chore: add backend service to docker-compose file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from http import HTTPStatus
 
+import uvicorn
 from fastapi import FastAPI
 
-from src.schemes.posts import Post
+from core.config import config
+from schemes.posts import Post
 
 
 app = FastAPI(redoc_url=None)
@@ -11,3 +13,9 @@ app = FastAPI(redoc_url=None)
 @app.post('/posts/', status_code=HTTPStatus.CREATED)
 async def create_post(post: Post):
     return {'message': 'Пост был отправлен на модерацию'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app', host=config.host, port=config.port, reload=config.debug
+    )
