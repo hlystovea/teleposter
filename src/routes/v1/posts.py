9948a887f713c1c7329c9c0586ca8626c0d01ca4
Evgenii Khlystov <hlystovea@gmail.com>
2024-04-07 14:05:36 +0700

    Add: get_posts endpoint

--- src/routes/v1/posts.py
+++ src/routes/v1/posts.py
@@ -2,11 +2,23 @@ from fastapi import (APIRouter, Depends, status)
 
 from core.messages import MSG
 from db.mongo import posts_collection
-from schemes.posts import PostCreatedResponse
+from schemes.posts import Post, PostCreatedResponse
 from schemes.telegram import TelegramMessage
 
 
 router = APIRouter(prefix='/api/v1/posts', tags=['posts'])
+
+
+@router.get(
+    '/',
+    response_model=list[Post],
+    tags=['posts'],
+    summary='get posts',
+    description='Respones list of posts',
+    name='v1:posts:post-list',
+)
+async def get_posts(posts=Depends(posts_collection)):
+    return await posts.find().to_list(1000)
 
 
 @router.post(
