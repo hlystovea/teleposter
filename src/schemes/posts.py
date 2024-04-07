4d8a1a772c98f78ba580b3340b7a519799e9104b
Evgenii Khlystov <hlystovea@gmail.com>
2024-04-07 16:10:01 +0700

    Fix: rename field create_at to created_at

--- src/schemes/posts.py
+++ src/schemes/posts.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel, Field
 class Post(BaseModel):
     user_name: str | None = None
     text: str | None = None
-    create_at: datetime = Field(default_factory=datetime.now)
+    created_at: datetime = Field(default_factory=datetime.now)
     status: Literal['non-moderated', 'published'] = 'non-moderated'
 
 
