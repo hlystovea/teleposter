f8b47f26f854e5d24b5af026dbea0f4128322d4e
Evgeniy Khlystov <hlystovea@gmail.com>
2024-04-12 09:50:57 +0700

    Add: post-publish method

--- src/routes/v1/posts.py
+++ src/routes/v1/posts.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from pymongo.results import DeleteResult, UpdateResult
 
 from core.messages import MSG
-from db.mongo import posts_collection
+from db.mongo import posts
 from schema.posts import Post, ResponsePost, ResponseMessage
 
 
@@ -18,7 +18,7 @@ router = APIRouter(prefix='/api/v1/posts', tags=['posts'])
     description='Responds with a list of posts',
     name='v1:posts:post-list',
 )
-async def get_posts(posts=Depends(posts_collection)):
+async def get_posts(posts=Depends(posts)):
     return await posts.find().sort(
         [
             ('status', 1),
@@ -35,7 +35,7 @@ async def get_posts(posts=Depends(posts_collection)):
     description='Creates a new non-moderated post',
     name='v1:posts:post-create',
 )
-async def create_post(post: Post, posts=Depends(posts_collection)):
+async def create_post(post: Post, posts=Depends(posts)):
     _ = await posts.insert_one(post.model_dump())
     return {'message': MSG.post_has_been_sent}
 
@@ -47,7 +47,7 @@ async def create_post(post: Post, posts=Depends(posts_collection)):
     description='Deletes the post',
     name='v1:posts:post-delete',
 )
-async def delete_post(post_id: str, posts=Depends(posts_collection)):
+async def delete_post(post_id: str, posts=Depends(posts)):
     result: DeleteResult = await posts.delete_one({'_id': ObjectId(post_id)})
 
     if not result.deleted_count:
@@ -63,7 +63,7 @@ async def delete_post(post_id: str, posts=Depends(posts_collection)):
     description='Updates the post',
     name='v1:posts:post-update',
 )
-async def update_post(post_id: str, post: Post, posts=Depends(posts_collection)):
+async def update_post(post_id: str, post: Post, posts=Depends(posts)):
     result: UpdateResult = await posts.update_one(
         filter={'_id': ObjectId(post_id)},
         update=post.model_dump(exclude={'id'})
@@ -73,3 +73,19 @@ async def update_post(post_id: str, post: Post, posts=Depends(posts_collection))
         raise HTTPException(status_code=404, detail='Post not found')
 
     return {'message': MSG.post_has_been_updated}
+
+
+@router.post(
+    '/{post_id}/publish',
+    response_model=ResponseMessage,
+    summary='publish post',
+    description='Publishes the post in a telegram channel',
+    name='v1:posts:post-publish',
+)
+async def publish_post(post_id: str, posts=Depends(posts)):
+    post = posts.find_one(filter={'_id': ObjectId(post_id)})
+
+    if not post:
+        raise HTTPException(status_code=404, detail='Post not found')
+
+    return {'message': MSG.post_has_been_published}
