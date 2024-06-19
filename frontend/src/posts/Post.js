cc8a96d47e5f9e5d0d03ef7bc425abf4dfa2c019
Evgeniy Khlystov <hlystovea@gmail.com>
2024-06-19 11:47:15 +0700

    Ref: move the buttons outside the element

--- frontend/src/posts/Post.js
+++ frontend/src/posts/Post.js
@@ -17,51 +17,7 @@ function Post({post}) {
       alt: file,
     };
   })
-  return (
-    <Card>
-      <Gallery photos={photos} />
-      {isEditing ? (
-        <EditForm post={post} setIsEditing={setIsEditing} />
-      ) : (
-        <PostCard post={post} setIsEditing={setIsEditing} />
-      )}
-    </Card>
-  );
-}
-
-const EditForm = ({post, setIsEditing}) => {
-  const [textValue, setTextValue] = useState(post.text || post.caption || '');
-  const rows = textValue.split('\n').length + 1;
   const updatePost = useUpdatePost();
-  const onCancelClick = () => {
-    setIsEditing(false);
-  }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
-    updatePost.mutate({postId: post.id, data: data});
-    setIsEditing(false);
-  };
-  const onTextChange = (event) => {
-    const input = event.target;
-    setTextValue(input.value);
-    input.style.height = '1px';
-    input.style.height = input.scrollHeight + 'px';
-  };
-  return (
-    <form action="" method="POST" onSubmit={onSubmit}>
-      <textarea className="text-input" rows={rows} name="text" value={textValue} onChange={onTextChange} autoFocus />
-      <button className="btn-save" name="saveButton" type="submit">
-          Сохранить
-      </button>
-      <button className="btn-cancel" name="cancelButton" type="button" onClick={onCancelClick}>
-          Отмена
-      </button>
-    </form>
-  );
-}
-const PostCard = ({post, setIsEditing}) => {
   const deletePost = useDeletePost();
   const publishPost = usePublishPost();
   const onPublishClick = () => {
@@ -73,18 +29,71 @@ const PostCard = ({post, setIsEditing}) => {
   const onDeleteClick = () => {
     deletePost.mutate(post.id);
   };
-  return (
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+    updatePost.mutate({postId: post.id, data: data});
+    setIsEditing(false);
+  };
+  const postButtons = (
     <>
-      <p className="post-text">{post.text || post.caption}</p>
-      <button className="btn-publish" name="publishButton" type="button" onClick={onPublishClick}>
+      <button className='btn-publish' name='publishButton' type='button' onClick={onPublishClick}>
         Опубликовать
       </button>
-      <button className="btn-edit" name="editButton" type="button" onClick={onEditClick}>
+      <button className='btn-edit' name='editButton' type='button' onClick={onEditClick}>
         Редактировать
       </button>
-      <button className="btn-delete" name="deleteButton" type="button" onClick={onDeleteClick}>
+      <button className='btn-delete' name='deleteButton' type='button' onClick={onDeleteClick}>
         Удалить
       </button>
+    </>
+  )
+  const formButtons = (
+    <>
+      <button form={post.id} className='btn-save' name='saveButton' type='submit'>
+        Сохранить
+      </button>
+      <button className='btn-cancel' name='cancelButton' type='button' onClick={() => setIsEditing(false)}>
+        Отмена
+      </button>
+    </>
+  )
+  return (
+    <Card>
+      <Gallery photos={photos} />
+      {isEditing ? (
+        <Form post={post} buttons={formButtons} onSubmit={onSubmit} />
+      ) : (
+        <Content post={post} buttons={postButtons} />
+      )}
+    </Card>
+  );
+}
+
+const Form = ({post, buttons, onSubmit}) => {
+  const [textValue, setTextValue] = useState(post.text || post.caption || '');
+  const rows = textValue.split('\n').length;
+  const onTextChange = (event) => {
+    const input = event.target;
+    setTextValue(input.value);
+    input.style.height = '1px';
+    input.style.height = input.scrollHeight + 'px';
+  };
+  return (
+    <>
+      <form id={post.id} onSubmit={onSubmit}>
+        <textarea className='text-input' rows={rows} name='text' value={textValue} onChange={onTextChange} autoFocus />
+      </form>
+      {buttons}
+    </>
+  );
+}
+const Content = ({post, buttons}) => {
+  return (
+    <>
+      <p className='post-text'>{post.text || post.caption}</p>
+      {buttons}
     </>
   );
 }
