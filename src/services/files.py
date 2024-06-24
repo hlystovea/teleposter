3b04bc18196cf4bf7b8e4249a066d4171eed5e08
Evgeniy Khlystov <hlystovea@gmail.com>
2024-06-24 14:51:10 +0700

    Ref: handle api requests via aiogram

--- src/services/files.py
+++ src/services/files.py
@@ -1,14 +1,13 @@
 import aiofiles
 import random
 import string
-from typing import AsyncGenerator
 
 from fastapi import UploadFile, HTTPException
 
 from core.config import config
 from core.logger import logger
 from schema.posts import Post
-from services.telegram import get_file, stream_file
+from services.telegram import get_file, download_file
 
 
 def generate_random_string(length: int = 16):
@@ -40,27 +39,19 @@ async def save_media(post: Post) -> Post:
         file_name = f'{generate_random_string()}.jpg'
 
         try:
-            async with aiofiles.open(media_dir / file_name, 'wb') as out_file:
-                streaming_file = stream_file(file.file_path)
-                async for chunk in streaming_file:
-                    await out_file.write(chunk)
-
+            await download_file(file.file_path, media_dir / file_name)
             post.files.append(file_name)
 
         except OSError as error:
             logger.error(f'An error occured: {error}')
             raise HTTPException(status_code=500, detail='Internal server error')
-    
+
     if post.video:
         file = await get_file(post.video.file_id)
         file_name = f'{generate_random_string()}.mp4'
 
         try:
-            async with aiofiles.open(media_dir / file_name, 'wb') as out_file:
-                streaming_file = stream_file(file.file_path)
-                async for chunk in streaming_file:
-                    await out_file.write(chunk)
-
+            await download_file(file.file_path, media_dir / file_name)
             post.files.append(file_name)
 
         except OSError as error:
--- src/services/telegram.py
+++ src/services/telegram.py
@@ -1,40 +1,33 @@
 import hashlib
 import hmac
+from pathlib import Path
 from typing import Any, AsyncGenerator
 
-from fastapi import HTTPException
-from httpx import AsyncClient, HTTPError
+from aiogram import Bot
+from aiogram.enums import ParseMode
+from aiogram.types import FSInputFile, File
+from aiogram.utils.media_group import MediaGroupBuilder, MediaType
+from httpx import AsyncClient
 
 from core.config import config
 from schema.posts import Post
-from schema.telegram import TelegramMedia, TelegramAuthData
+from schema.telegram import TelegramAuthData
 
 
-def get_api_url(method: str) -> str:
-    bot_token = config.bot_token.get_secret_value()
-    return f'https://api.telegram.org/bot{bot_token}/{method}'
+bot = Bot(
+    token=config.bot_token.get_secret_value(),
+    parse_mode=ParseMode.MARKDOWN_V2
+)
 
 
 async def publish_in_channel(post: Post) -> None:
-    headers = {'Content-Type': 'application/json'}
-    data = {'chat_id': config.channel}
+    chat_id = config.channel
 
-    if post.photo:
-        url = get_api_url('sendPhoto')
-        data |= {'photo': post.photo[0].file_id, 'caption': post.caption}
-    elif post.video:
-        url = get_api_url('sendVideo')
-        data |= {'video': post.video.file_id, 'caption': post.caption}
-    else:
-        url = get_api_url('sendMessage')
-        data |= {'text': post.text}
+    if post.files:
+        await bot.send_media_group(chat_id, await create_album(post))
+        return
 
-    async with AsyncClient() as client:
-        response = await client.post(url, headers=headers, json=data)
-        data = response.json()
-
-    if not data['ok']:
-        raise HTTPError(f'Response: {data}')
+    await bot.send_message(chat_id, post.text or '')
 
 
 def check_auth_data(auth_data: TelegramAuthData) -> bool:
@@ -47,37 +40,8 @@ def check_auth_data(auth_data: TelegramAuthData) -> bool:
     return hmac.compare_digest(auth_data_hash, auth_data.hash)
 
 
-async def get_admin_ids() -> list[int]:
-    headers = {'Content-Type': 'application/json'}
-    data = {'chat_id': config.channel}
-    url = get_api_url('getChatAdministrators')
-
-    async with AsyncClient() as client:
-        response = await client.post(url, headers=headers, json=data)
-        data = response.json()
-
-    if not data['ok']:
-        raise HTTPError(f'Response: {data}')
-
-    return [member['user']['id'] for member in data['result']]
-
-
-async def get_file(file_id: str) -> TelegramMedia:
-    headers = {'Content-Type': 'application/json'}
-    data = {'file_id': file_id}
-    url = get_api_url('getFile')
-
-    async with AsyncClient() as client:
-        response = await client.post(url, headers=headers, json=data)
-        data = response.json()
-
-    if not data['ok']:
-        raise HTTPException(
-            status_code=data['error_code'],
-            detail=data['description']
-        )
-
-    return TelegramMedia(**data['result'])
+async def get_file(file_id: str) -> File:
+    return await bot.get_file(file_id)
 
 
 async def stream_file(
@@ -90,3 +54,16 @@ async def stream_file(
         async with client.stream('GET', url) as response:
             async for chunk in response.aiter_bytes(chunk_size):
                 yield chunk
+
+
+async def download_file(file_path: str, destination: str | Path) -> None:
+    await bot.download_file(file_path, destination)
+
+
+async def create_album(post: Post) -> list[MediaType]:
+    album_builder = MediaGroupBuilder(caption=post.text)
+
+    for file in post.files:
+        album_builder.add_photo(media=FSInputFile(config.media_root / file))
+
+    return album_builder.build()
