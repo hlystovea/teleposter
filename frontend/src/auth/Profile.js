dcc399fe371c492ef95122d68794bbb0679f0048
Evgeniy Khlystov <hlystovea@gmail.com>
2024-07-10 16:54:34 +0700

    Ref: delete the isOpenMenu condition check

--- frontend/src/auth/Profile.js
+++ frontend/src/auth/Profile.js
@@ -14,13 +14,11 @@ function Profile({profile, logout}) {
     return (
       <div className='profile'>
         <ProfileLogo profile={profile} onClick={onClick} />
-        {isOpenMenu && (
-          <ul className={`profile__menu ${isOpenMenu ? 'show' : ''}`}>
-            <li>{profile.first_name} {profile.last_name}</li>
-            <li>@{profile.username}</li>
-            <li className='profile__menu__item' onClick={onLogout}><LogoutItem /></li>
-          </ul>
-        )}
+        <ul className={`profile__menu ${isOpenMenu ? 'show' : ''}`}>
+          <li>{profile.first_name} {profile.last_name}</li>
+          <li>@{profile.username}</li>
+          <li className='profile__menu__item' onClick={onLogout}><LogoutItem /></li>
+        </ul>
       </div>
     )
 }
